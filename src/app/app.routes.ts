0fa7e5e01a092de75938f5305f6b65c5ae85f881
Ketlen Possoli <possoli.ketlen@gmail.com>
2024-08-12 16:19:36 -0300

    Formulário de Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // { path: 'inicio', component: InicioComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule,
+    BrowserModule
+  ],
+  exports: [RouterModule],
+  declarations: [
+    LoginComponent
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  users = [
+    { email: 'admin@example.com', password: 'admin123', role: 'Administrador' },
+    { email: 'docente@example.com', password: 'docente123', role: 'Docente' },
+    { email: 'aluno@example.com', password: 'aluno123', role: 'Aluno' }
+  ];
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    const user = this.users.find(u => u.email === this.email && u.password === this.password);
+    if (user) {
+      alert(`Bem-vindo, ${user.role}`);
+      this.router.navigate(['/inicio']);
+    } else {
+      alert('Usuário e/ou senha incorretos');
+    }
+  }
+
+  showUnderConstruction(feature: string) {
+    alert(`A funcionalidade "${feature}" está em construção.`);
+  }
+}
